720b5b7282550b1a45bfd3f44400f81ae0bbdcba
pttiwari11 <pushpendra.tiwari_cs19@gla.ac.in>
2022-08-20 15:48:55 +0530

    make it awesome

--- backend/server.js
+++ backend/server.js
@@ -130,7 +130,11 @@ if(process.env.NODE_ENV == "production") {
   const path = require("path");
   app.get("*", (req,res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
+  });
+} else {
+  app.get("*", (req, res) => {
+    res.send("Api running");
+  });
 }
 
 app.listen(PORT);
